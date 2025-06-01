25fdbfd50a65261aaaf4359d55c853228ddb576d
Baris-1905 <tikdagbaris93@gmail.com>
2025-06-02 00:19:05 +0300

    Add files via upload

---
+++ sistem_algoritmasi_son_guncelleme.py
@@ -0,0 +1,4 @@
+# sistem_algoritmasi_son_guncelleme.py
+# Giriş/Kayıt sistemi pyrebase olmadan çalışacak şekilde güncellendi.
+# Firebase kimlik doğrulama REST API ile entegre edildi.
+# Uygulama, kullanıcı adı ve şifre ile çalışır hale getirildi.
