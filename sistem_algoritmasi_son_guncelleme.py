feed0910a96392eda0d7f8f93656b926167abc6e
Baris-1905 <tikdagbaris93@gmail.com>
2025-06-02 00:31:21 +0300

    Delete sistem_algoritmasi_son_guncelleme.py

--- sistem_algoritmasi_son_guncelleme.py
+++
@@ -1,4 +0,0 @@
-# sistem_algoritmasi_son_guncelleme.py
-# Giriş/Kayıt sistemi pyrebase olmadan çalışacak şekilde güncellendi.
-# Firebase kimlik doğrulama REST API ile entegre edildi.
-# Uygulama, kullanıcı adı ve şifre ile çalışır hale getirildi.
